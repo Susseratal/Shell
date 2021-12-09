e1c2f5ec36f736d279348c24f6ebb0cbea037fb0
Iain <jtfjyrx@gmail.com>
2021-12-09 10:59:13 +0000

    initial commit

---
+++ rice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# I'm gonna play with this some more
+
+st -f SourceCodePro:pixelsize=13 &
+st gotop &
+st vis &
+st cmatrix
--- talking.sh
+++ talking.sh
@@ -5,3 +5,5 @@ vis=$! &
 sleep 2 && 
 espeak-ng -g 15 "Computer online" && 
 pkill $vis
+
+# last step is to get it to kill vis afterwards, then I'm basically set
